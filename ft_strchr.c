aec105f07615ae4ffddb242c524ca2bcff4c67b7
rdragan <rdragan@student.42heilbronn.de>
2022-11-09 12:47:48 +0100

    finished

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 12:27:51 by rdragan           #+#    #+#             */
-/*   Updated: 2022/11/09 12:27:33 by rdragan          ###   ########.fr       */
+/*   Updated: 2022/11/09 12:47:05 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 Returns the address of the first occurence of c in s.
 If c is '\0' then locates it's address in s.
 If not found, returns NULL.
+If c > 255 start the count over again to prevent overflow.
 */
 char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
 		return (NULL);
-	if (c == '\0' || !ft_isascii(c))
+	if (c % 255 != 0)
 		return ((char *)s + ft_strlen(s));
 	while (*s)
 	{
