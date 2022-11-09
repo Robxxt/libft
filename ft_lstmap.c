e060287345fadbfa5febb22f51f2cc8636f5dd46
rdragan <rdragan@student.42heilbronn.de>
2022-11-09 17:14:34 +0100

    created & protected

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 11:16:28 by rdragan           #+#    #+#             */
-/*   Updated: 2022/11/09 09:56:01 by rdragan          ###   ########.fr       */
+/*   Updated: 2022/11/09 17:13:51 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,25 @@ is used to delete the content of a node if needed.
 */
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	return (NULL);
+	t_list	*new_lst;
+	t_list	*new_node;
+
+	if ((!lst) || !f)
+		return (NULL);
+	new_lst = ft_lstnew(f(lst->content));
+	if (!new_lst)
+		return (NULL);
+	lst = lst->next;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
