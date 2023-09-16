f3a4ef909ba693234679a7690f0ad6f9d8067377
rdragan <rdragan@student.42heilbronn.de>
2023-09-16 23:17:58 +0200

    fuck

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 10:44:10 by rdragan           #+#    #+#             */
-/*   Updated: 2023/04/26 15:38:08 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/09/16 23:17:16 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	ft_lstdelone(t_list *lst)
 {
 	if (!lst)
 		return ;
+	free(lst->content);
 	free(lst);
 }
